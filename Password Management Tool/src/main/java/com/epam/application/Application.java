348686b761e2f7485d259b446bcb0badcc83f0d8
Manash Rauta <manash_rauta@epam.com>
2021-12-10 03:22:55 +0530

    Spring Boot Implemented - done

--- Password Management Tool/src/main/java/com/epam/application/Application.java
+++ Password Management Tool/src/main/java/com/epam/application/Application.java
@@ -7,16 +7,12 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.epam")
 public class Application implements ApplicationRunner
 {
-    @Autowired
-    ApplicationContext context;
-
     @Autowired
     HomeMenu homeMenu;
 
