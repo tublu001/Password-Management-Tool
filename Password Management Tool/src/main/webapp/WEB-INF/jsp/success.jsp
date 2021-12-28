6c1fa0ea401736ea0d91205580002cfce44dd67c
Manash Rauta <manash_rauta@epam.com>
2021-12-28 08:05:03 +0530

    Spring mvc + security

--- Password Management Tool/src/main/webapp/WEB-INF/jsp/success.jsp
+++
@@ -1,7 +0,0 @@
-<%@ include file="header.jsp" %>
-<body>
-
-<h2>Success Page</h2>
-<br>
-<a href="/PMT/" >Login Menu</a>
-<%@ include file="footer.jsp" %>
--- Password Management Tool/src/test/java/com/epam/AppTest.java
+++
@@ -1,9 +0,0 @@
-package com.epam;
-
-public class AppTest
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
-    }
-}
